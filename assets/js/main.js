39460c30b5814679d4565af0cc5fa8b9fda9b604
shanto-dev <shantoroy1792@gmail.com>
2024-11-16 08:52:57 +0600

    update all Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,17 @@ var bramdSwiper = new Swiper('.brand_carousel', {
     }
 });
 
+// Testimonial 
+var testimonialSwiper = new Swiper('.cStudies_testimonial_carousel', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+});
+
 $(".countfact").appear();
 $(document.body).on("appear", ".countfact", function (e, $affected) {
     $affected.each(function () {
@@ -60,3 +71,57 @@ $(document.body).on("appear", ".countfact", function (e, $affected) {
         }
     });
 });
+
+var currentStep = 1;
+var updateProgressBar;
+
+function displayStep(stepNumber) {
+    if (stepNumber >= 1 && stepNumber <= 3) {
+        $(".step-" + currentStep).hide();
+        $(".step-" + stepNumber).show();
+        currentStep = stepNumber;
+        updateProgressBar();
+        updateActiveStepClass();
+    }
+}
+
+function updateActiveStepClass() {
+    $('.step-circle').removeClass('active');
+    $('.step-circle').eq(currentStep - 1).addClass('active');
+}
+
+$('#nda-multi-step-form').find('.step').slice(1).hide();
+
+$(".next-step").click(function () {
+    if (currentStep < 3) {
+        $(".step-" + currentStep).addClass("animate__animated animate__fadeOut");
+        currentStep++;
+        setTimeout(function () {
+            $(".step").removeClass("animate__animated animate__fadeOut").hide();
+            $(".step-" + currentStep).show().addClass("animate__animated animate__fadeIn");
+            updateProgressBar();
+            updateActiveStepClass();
+        }, 500);
+    }
+});
+
+$(".prev-step").click(function () {
+    if (currentStep > 1) {
+        $(".step-" + currentStep).addClass("animate__animated animate__fadeOut");
+        currentStep--;
+        setTimeout(function () {
+            $(".step").removeClass("animate__animated animate__fadeOut").hide();
+            $(".step-" + currentStep).show().addClass("animate__animated animate__fadeIn");
+            updateProgressBar();
+            updateActiveStepClass();
+        }, 500);
+    }
+});
+
+updateProgressBar = function () {
+    var progressPercentage = ((currentStep - 1) / 2) * 100;
+    $(".progress-bar").css("width", progressPercentage + "%");
+};
+
+// Initialize active step on load
+updateActiveStepClass();
